0ad0fc456d18a30a890f2aa71b7764e6fce97aa6
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-23 21:28:13 +0000

    JavaFX application called MapDisplay created

---
+++ DiamondHunter/src/com/neet/DiamondHunter/GameState/MapDisplay.java
@@ -0,0 +1,18 @@
+package com.neet.DiamondHunter.GameState;/**
+ * Created by Kesava on 23/11/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MapDisplay extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
