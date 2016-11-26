9c378b3807cad7f69df37aa0a3f5d34537d75a0d
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-26 17:45:15 +0000

    Trying to load mapviewer.fxml

--- MapViewerFX/MapViewer/src/sample/Main.java
+++ MapViewerFX/MapViewer/src/sample/Main.java
@@ -10,10 +10,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mapviewer.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root,600,600);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("mapviewer.fxml"));
+            root.setCenter(content);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
