245f1bc23f115012ef2b625897b01ae5a7f08130
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-11-26 17:51:15 +0000

    Refactored name of package and xml file

--- MapViewerFX/MapViewer/src/Map/Controller.java
+++ MapViewerFX/MapViewer/src/Map/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package Map;
 
 public class Controller {
 }
--- MapViewerFX/MapViewer/src/Map/Main.java
+++ MapViewerFX/MapViewer/src/Map/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Map;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
