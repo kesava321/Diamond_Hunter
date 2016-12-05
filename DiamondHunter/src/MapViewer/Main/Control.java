de428b67a4f96808a7e4cbd0d07984c6dfd0bdbd
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-12-05 20:01:27 +0000

    Updated to make map window open

---
+++ DiamondHunter/src/MapViewer/Main/Control.java
@@ -0,0 +1,34 @@
+package MapViewer.Main;/**
+ * Created by Kesava on 04/12/2016.
+ */
+public class Control {
+/*
+        import javafx.fxml.FXML;
+        import javafx.scene.image.Image;
+
+public class Control {
+
+    MapModel mp = new MapModel();
+
+    public void initialize() {
+        Image image = new Image("testtileset.gif");
+        logoIV.setImage(image);
+
+    }
+
+    @FXML public void LoadMapPressed() {
+        mp.add("testmap.map");
+        updateMapDisplay();
+
+    }
+
+    @FXML public void LoadTilePressed() {
+        mp.add("testtileset.gif");
+        updateTileDisplay();
+    }
+
+
+*/
+
+
+}
---
+++ DiamondHunter/src/MapViewer/Main/MapView.java
@@ -0,0 +1,86 @@
+package MapViewer.Main;/**
+ * Created by Kesava on 04/12/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MapView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+    /*
+    public MapView (int width, int height)
+    {
+        map = new int[height][width];
+    }
+    public static MapView FromFile(String fileName)
+    {
+        MapView layer = null;
+        ArrayList<ArrayList<Integer>> tempLayout = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+        {
+            String currentLine;
+
+            while((currentLine = br.readLine())!=null)
+            {
+                if(currentLine.isEmpty())
+                    continue;
+                ArrayList<Integer> row = new ArrayList<>();
+                String[] values = currentLine.trim().split ("");
+                for (String string : values)
+                {
+                    if(!string.isEmpty())
+                    {
+                        int id = Integer.parseInt(string);
+                        row.add(id);
+                    }
+                }
+                tempLayout.add(row);
+            }
+        }
+        catch(IOException e)
+        {
+
+        }
+        int width = tempLayout.get(0).size();
+        int height= tempLayout.size();
+
+        layer = new MapView(width, height);
+
+        for(int y=0;y<height;y++)
+        {
+            for(int x=0;x<width;x++)
+            {
+                layer.map[y][x] = tempLayout.get(y).get(x);
+            }
+        }
+        layer.tileSheet = layer.LoadTileSheet("stackedTileSheet.jpg");
+
+                return layer;
+    }
+    public DrawPanel()
+    {
+        layer = TileLayer.FromFile("map.txt");
+    }
+
+    @Override public void paintComponent (Graphics g)
+    {
+        super.paintComponent(g);
+        layer.DrawLayer(g);
+    }
+*/
+}
