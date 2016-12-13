fcec041aeabbe4796e9972acc7196ca7cbe4838b
Kesava Uthaya-kumaran <Kesava Uthaya-kumaran>
2016-12-13 19:21:46 +0000

    Added variable button to represent selected item.

--- DiamondHunter/src/MapViewer/Main/Control.java
+++ DiamondHunter/src/MapViewer/Main/Control.java
@@ -1,73 +1,38 @@
 package MapViewer.Main;
-/*
-import com.neet.DiamondHunter.TileMap.TileMap;
+
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 
 
 public class Control {
 
-    Control mp = new Control ();
-    @FXML
-    ImageView tileset;
+	private static String button = "";
+	
+	@FXML Button axeButton;
+	@FXML Button boatButton;
+	@FXML public void onAxeButtonPressed() {
+		
+		setButton("Axe");
+		
+	}
+	@FXML public void onBoatButtonPressed() {
+		setButton("Boat");
+	}
 
-    public void initialize() {
-        Image image = new Image("testtileset.gif");
-        tileset.setImage(image);
-
-    }
-
-    @FXML public void LoadMapPressed() {
-        mp.add("testmap.map");
-        updateMapDisplay();
-
-    }
-
-    @FXML public void LoadTilePressed() {
-        mp.add("testtileset.gif");
-        updateTileDisplay();
-    }
-
-
-
+	
+	public static void setButton(String s){
+		button = s;
+	}
+	
+	public static String getButton(){
+		return button;
+	}
+	
+	
+	
+	
+    
 }
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-/*
-public class Control extends Application {
-    private TileMap tileMap;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Map Viewer");
-        Button btn = new Button();
-        btn.setText("Load Map'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //System.out.println("Map Viewer");
-                tileMap = new TileMap(40);
-                tileMap.loadTiles("/Tilesets/testtileset.gif");
-                tileMap.loadMap("/Maps/testmap.map");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
-}
-*/
 
 		
